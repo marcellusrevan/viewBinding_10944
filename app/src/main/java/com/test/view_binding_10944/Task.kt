8f784984280440cf25682292b045dd85ba38a97e
marcellusrevan <marcellusrevan123@gmail.com>
2022-09-13 09:33:23 +0700

    guided

---
+++ app/src/main/java/com/test/view_binding_10944/Task.kt
@@ -0,0 +1,4 @@
+package com.test.view_binding_10944
+
+data class Task(val chapter:String, val title:String, val details:String) {
+}
---
+++ app/src/main/java/com/test/view_binding_10944/TaskList.kt
@@ -0,0 +1,11 @@
+package com.test.view_binding_10944
+
+object TaskList {
+    val taskList= listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur","Kebiasaan Sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2","Nguli Tower Of Fantasy","Mencari Dark Crystal untuk gacha Frigg wangy"),
+        Task("Chapter 3","Ngopi","Habis nguli enaknya ngopi"),
+        Task("Chapter 4","Dimana aku?","Disini gelap gabisa liat"),
+        Task("Chapter 5","Upload ke FB","Jual akun Tower Of Fantasy")
+    )
+}
